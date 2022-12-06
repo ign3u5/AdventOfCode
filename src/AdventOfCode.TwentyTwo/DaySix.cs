b8f2bb2fe6ce6ee53fb3de30051dbc7281221a01
Chris <chris.farmer@justeattakeaway.com>
2022-12-06 10:33:53 +0000

    Day 6 Task 1 complete

--- src/AdventOfCode.TwentyTwo/DaySix.cs
+++ src/AdventOfCode.TwentyTwo/DaySix.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using AdventOfCode.Domain;
+﻿using AdventOfCode.Domain;
 
 namespace AdventOfCode.TwentyTwo;
 
@@ -8,6 +7,25 @@ public class DaySix : IChallenge<int>
     public string ChallengeInputPath => Path.GetFullPath("./ChallengeInputData/day_6.txt");
     public int Run(string[] lines)
     {
-        return lines.Length;
+        string line = lines[0];
+        HashSet<char> differentContiguousChars = new();
+        var numberOfDifferentChars = 0;
+        
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (differentContiguousChars.TryGetValue(line[i], out _))
+            {
+                differentContiguousChars = new() { line[i] };
+                numberOfDifferentChars = 1;
+                continue;
+            }
+
+            differentContiguousChars.Add(line[i]);
+            numberOfDifferentChars++;
+            if (numberOfDifferentChars == 4)
+                return i + 1;
+        }
+
+        return 0;
     }
 }
