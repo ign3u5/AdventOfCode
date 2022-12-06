97068d9edef4a795e0a5d393eb833069a72044d3
Chris Farmer <chrisfarmer19801@gmail.com>
2022-12-06 18:56:45 +0000

    Add template for Day 5 + input

---
+++ src/AdventOfCode.TwentyTwo/DayFive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AdventOfCode.Domain;
+
+namespace AdventOfCode.TwentyTwo;
+
+public class DayFive : IChallenge<int>
+{
+    public string ChallengeInputPath => throw new NotImplementedException();
+
+    public int RunTaskOne(string[] lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int RunTaskTwo(string[] lines)
+    {
+        throw new NotImplementedException();
+    }
+}
