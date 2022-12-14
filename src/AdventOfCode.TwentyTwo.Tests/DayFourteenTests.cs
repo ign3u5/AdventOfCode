ab041bc17e30cca5d95ea6995692296e240bd280
Chris <chris.farmer@justeattakeaway.com>
2022-12-14 18:55:30 +0000

    WIP - Day 14 Task 1

---
+++ src/AdventOfCode.TwentyTwo.Tests/DayFourteenTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace AdventOfCode.TwentyTwo.Tests;
+
+public class DayFourteenTests
+{
+    private const string _input = """
+        498,4 -> 498,6 -> 496,6
+        503,4 -> 502,4 -> 502,9 -> 494,9
+        """;
+    
+    [Theory]
+    [InlineData(_input, 21)]
+    public void RunTaskOne(string input, int expected)
+    {
+        // Arrange
+        var lines = input.ReplaceLineEndings("\n").Split("\n");
+        var sut = new DayFourteen();
+        
+        // Act
+        var settledSand = sut.RunTaskOne(lines);
+        
+        // Assert
+        settledSand.Should().Be(expected);
+    }
+}
---
+++ src/AdventOfCode.TwentyTwo/DayFourteen.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode.Domain;
+
+namespace AdventOfCode.TwentyTwo;
+
+public class DayFourteen : IChallenge<int>
+{
+    public string ChallengeInputPath => DataProvider<DayFourteen>.GetChallengePath();
+    public int RunTaskOne(string[] lines)
+    {
+        return 0;
+    }
+
+    public int RunTaskTwo(string[] lines)
+    {
+        return 0;
+    }
+}
