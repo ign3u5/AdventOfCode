5241138f5e4bc1064c3b6bf1e72b34bab8fa0583
Chris Farmer <chrisfarmer19801@gmail.com>
2022-12-15 09:18:45 +0000

    WIP - Day 14

--- src/AdventOfCode.TwentyTwo/DayFourteen.cs
+++ src/AdventOfCode.TwentyTwo/DayFourteen.cs
@@ -5,8 +5,17 @@ namespace AdventOfCode.TwentyTwo;
 public class DayFourteen : IChallenge<int>
 {
     public string ChallengeInputPath => DataProvider<DayFourteen>.GetChallengePath();
+
     public int RunTaskOne(string[] lines)
     {
+        var coords = lines.Select(l => l.Split(" -> ")
+            .Select(rawCoords => rawCoords.Split(',').ToArray())
+            .Select(a => (a[0], a[1])).ToArray())
+            .ToArray();
+
+        var notAtRest = true;
+
+
         return 0;
     }
 
